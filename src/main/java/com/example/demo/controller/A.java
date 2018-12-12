f177a5c82f829e5dca5df8caf373a82d7f0f3273
1360244065 <40311272+1360244065@users.noreply.github.com>
2018-12-12 17:57:39 +0800

    Update A.java

--- src/main/java/com/example/demo/controller/A.java
+++ src/main/java/com/example/demo/controller/A.java
@@ -5,6 +5,6 @@ package com.example.demo.controller;
  */
 public class A {
     public static void main(String[] args){
-        System.out.println("hello 在远程上修改");
+        System.out.println("hello 在远程上修改2");
     }
 }
