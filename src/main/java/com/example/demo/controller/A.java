cb574a651102fb24e1365e3b79c990bbfc9836c6
wjl <1360244065@qq.com>
2018-12-12 17:54:00 +0800

    test add commit push

--- src/main/java/com/example/demo/controller/A.java
+++ src/main/java/com/example/demo/controller/A.java
@@ -4,4 +4,7 @@ package com.example.demo.controller;
  * Created by 13602 on 2018/12/12.
  */
 public class A {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
 }
